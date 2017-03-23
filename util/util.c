efc7e0dd49d6239348462cea298a6e72da18a587
Maxwell A. Borden <maxwellborden@gmail.com>
2017-03-23 08:02:27 -0700

    going to class

--- util/util.c
+++ util/util.c
@@ -199,3 +199,27 @@ void log_event(event_type e, packet pkt) {
  * Functions for apache PRIO Q
  */
 
+static int cmp_pri(pqueue_pri_t next, pqueue_pri_t curr) {
+  return (next < curr);
+}
+
+
+static pqueue_pri_t get_pri(void *a) {
+  return ((node_t *) a)->pri;
+}
+
+
+static void set_pri(void *a, pqueue_pri_t pri) {
+  ((node_t *) a)->pri = pri;
+}
+
+
+static size_t get_pos(void *a) {
+  return ((node_t *) a)->pos;
+}
+
+
+static void set_pos(void *a, size_t pos) {
+  ((node_t *) a)->pos = pos;
+}
+
